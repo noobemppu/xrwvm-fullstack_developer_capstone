f3b1607df3f8e5a403ae4e7c4c6ab3f7bef5c430
EP <emil.perkkio@hotmail.fi>
2025-03-09 10:22:10 -0400

    Car models for backend

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -14,6 +14,7 @@ import logging
 import json
 from django.views.decorators.csrf import csrf_exempt
 from .populate import initiate
+from .models import CarMake, CarModel
 
 
 # Get an instance of a logger
@@ -21,7 +22,17 @@ logger = logging.getLogger(__name__)
 
 
 # Create your views here.
-
+def get_cars(request):
+    count = CarMake.objects.filter().count()
+    print(count)
+    if(count == 0):
+        initiate()
+    car_models = CarModel.objects.select_related('car_make')
+    cars = []
+    for car_model in car_models:
+        cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
+    return JsonResponse({"CarModels":cars})
+    
 # Create a `login_request` view to handle sign in request
 @csrf_exempt
 def login_user(request):
@@ -76,6 +87,7 @@ def registration(request):
     else :
         data = {"userName":username,"error":"Already Registered"}
         return JsonResponse(data)
+
 
 # # Update the `get_dealerships` view to render the index page with
 # a list of dealerships
