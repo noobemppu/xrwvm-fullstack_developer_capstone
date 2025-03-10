ea78cce090ac02b832c0e2b18bece5c198eceafb
noobemppu <159306293+noobemppu@users.noreply.github.com>
2025-03-10 11:42:49 +0200

    Update 0001_initial.py

--- server/djangoapp/migrations/0001_initial.py
+++ server/djangoapp/migrations/0001_initial.py
@@ -17,7 +17,7 @@ class Migration(migrations.Migration):
             name='CarMake',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True,
-                                           serialize=False, verbose_name='ID')),
+                                    serialize=False, verbose_name='ID')),
                 ('name', models.CharField(max_length=100)),
                 ('description', models.TextField()),
             ],
@@ -26,16 +26,20 @@ class Migration(migrations.Migration):
             name='CarModel',
             fields=[
                 ('id', models.BigAutoField(auto_created=True,
-                        primary_key=True, serialize=False, verbose_name='ID')),
+                primary_key=True, serialize=False, verbose_name='ID')),
                 ('name', models.CharField(max_length=100)),
-                ('type', models.CharField(choices=[('SEDAN', 'Sedan'), ('SUV', 'SUV'),
-                                ('WAGON', 'Wagon'), ('BUS', 'Bus')], max_length=50)),
+                ('type', models.CharField(choices=[
+                    ('SEDAN', 'Sedan'), ('SUV', 'SUV'),
+                    ('WAGON', 'Wagon'),
+                    ('BUS', 'Bus')
+                ], max_length=50)),
                 ('year', models.IntegerField(default=2025, validators=[
                     django.core.validators.MaxValueValidator(2025),
                     django.core.validators.MinValueValidator(2000)
                 ])),
-                ('car_make', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE,
-                                        to='djangoapp.carmake')),
+                ('car_make', 
+                models.ForeignKey(on_delete=django.db.models.deletion.CASCADE,
+                to='djangoapp.carmake')),
             ],
         ),
     ]
